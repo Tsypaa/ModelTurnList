540af49a811a5d8b4850cc0fb816f0ad5333a7c2
Artem <artwot77@gmail.com>
2024-12-15 22:09:16 +0300

    Some changes

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO("best.pt")
+class ModelSegment:
+    def __init__(self, pt = "best.pt", img = 'test_image.jpg'):
+        self.best_w = pt
+        self.image_path = img
 
-image_path = "test_image.jpg" 
+    def segment_turnlist(self):
+        model = YOLO(self.best_w)
 
-try:
-    img = cv2.imread(image_path)
-    if img is None:
-        raise FileNotFoundError(f"Image not found at {image_path}")
+        try:
+            img = cv2.imread(self.image_path)
+            if img is None:
+                raise FileNotFoundError(f"Image not found at {self.image_path}")
 
-    results = model.predict(img)
+            results = model.predict(img)
 
-    annotated_image = results[0].plot()
+            annotated_image = results[0].plot()
 
-    desired_width = 1080
-    desired_height = 720
-    annotated_image = cv2.resize(annotated_image, (desired_width, desired_height))
-    cv2.imshow("YOLO11 Inference", annotated_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+            desired_width = 1080
+            desired_height = 720
+            annotated_image = cv2.resize(annotated_image, (desired_width, desired_height))
+            cv2.imshow("YOLO11 Inference", annotated_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-except FileNotFoundError as e:
-    print(f"Error: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import ModelSegment
+
+model = ModelSegment(img="test_image2.jpg")
+
+model.segment_turnlist()
